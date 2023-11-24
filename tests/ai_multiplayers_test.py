be1bc029731b4023deb1c2d9924ad926a20f931b
Fabian Weissenbacher <40057494+f-weissenbacher@users.noreply.github.com>
2023-11-24 18:34:53 +0100

    Playing around with NstepPlanPlayer

--- tests/ai_multiplayers_test.py
+++ tests/ai_multiplayers_test.py
@@ -8,15 +8,15 @@ import random
 
 #random.seed(1234)
 
-game = AchtungDieKurveGame(mode="gui")
+game = AchtungDieKurveGame(mode="gui", target_fps=25)
 
 #for k in range(1,4):
 #    game.spawn_player(k, player_type=RandomSteeringAIPlayer, min_turn_radius=game.min_turn_radius,
 #                      turn_angles=np.deg2rad([20.,260.]))
 
 game.spawn_player(4, player_type="human", name="Testuser")
-#game.spawn_player(5, player_type=WallAvoidingAIPlayer, min_turn_radius=game.min_turn_radius)
-game.spawn_player(6, player_type=NStepPlanPlayer, name="HeuristicAgent", num_steps=2, dist_per_step=55.)
+game.spawn_player(5, player_type=WallAvoidingAIPlayer, min_turn_radius=game.min_turn_radius)
+game.spawn_player(6, player_type=NStepPlanPlayer, name="", num_steps=2, dist_per_step=55.)
 
 game.run_game_loop(close_when_finished=False)
 game.print_scoreboard()
--- tests/nstep_plan_player_test.py
+++ tests/nstep_plan_player_test.py
@@ -1,10 +1,12 @@
 import logging
 #import random
+import time
 
 import numpy as np
 seed = np.random.randint(1,20000)
 #seed = 16834
 #seed = 19421
+seed = 13798   # very nice at 30 fps
 print("Seed: ", seed)
 np.random.seed(seed)
 
@@ -12,13 +14,11 @@ np.random.seed(seed)
 from game import AchtungDieKurveGame
 from players.aiplayers import NStepPlanPlayer, WallAvoidingAIPlayer
 
-import pygame
-
 
 logging.basicConfig(level=logging.INFO,
                     format="%(relativeCreated)d %(levelname)s [%(module)s.%(funcName)s:%(lineno)d] - %(message)s")
 
-game = AchtungDieKurveGame(target_fps=20, game_speed_factor=1.0, run_until_last_player_dies=True,
+game = AchtungDieKurveGame(target_fps=30, game_speed_factor=1.0, run_until_last_player_dies=True,
                            ignore_self_collisions=False, mode="gui-debug")
 
 #print(f"Minimal turning radius: {game.min_turn_radius}")
@@ -35,7 +35,7 @@ game = AchtungDieKurveGame(target_fps=20, game_speed_factor=1.0, run_until_last_
 dist_per_step = 55.
 ticks_per_step = int(dist_per_step/game.dist_per_tick)
 p_ut = game.spawn_player(2, init_pos=(400,200), init_angle=np.deg2rad(0.), player_type=NStepPlanPlayer, num_steps=2,
-                         ticks_per_step=ticks_per_step, startblock_length=50, plan_update_period=int(1.2*ticks_per_step))
+                         ticks_per_step=ticks_per_step, startblock_length=50, plan_update_period=int(0.6*ticks_per_step))
 
 #p_ut = game.players[0] # player under test
 
@@ -46,7 +46,7 @@ p_ut = game.spawn_player(2, init_pos=(400,200), init_angle=np.deg2rad(0.), playe
 #R_min = game.min_turn_radius
 #center_rect = pygame.rect.Rect(2*R_min, 2*R_min, game.screen_width - 4*R_min, game.screen_height - 4*R_min)
 
-max_ticks = 50000
+max_ticks = 5000
 tick = 0
 game.running = True
 while game.running and tick <= max_ticks:
@@ -63,11 +63,11 @@ if game.running:
 else:
     logging.warning(f"FAILURE: {p_ut} hit an obstacle  (seed: {seed})")
 
-logging.info(f"Total travel distance: {p_ut.dist_travelled:.1f}")
+time.sleep(0.5)
+game.print_scoreboard()
 
 print("Number of plan  updates", p_ut.num_updates)
 
-game.running = True
 game.wait_for_window_close()
 
 
