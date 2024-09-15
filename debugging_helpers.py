50f34d0d0d2d118b5255373c296f08db93dc972a
Fabian Weissenbacher <40057494+f-weissenbacher@users.noreply.github.com>
2024-09-15 18:23:21 +0200

    improved plot_shapely_object

--- debugging_helpers.py
+++ debugging_helpers.py
@@ -1,14 +1,28 @@
 import matplotlib.pyplot as plt
 
-def plot_shapely_object(shp, ax=None, show=False):
+def plot_shapely_object(shp, color=None, ax=None, show=False, invert_y=False):
     import geopandas as gpd
-    gs = gpd.GeoSeries(shp)
-    p = gs.plot(ax=ax)
 
-    if show and ax is None:
+    if ax is None:
+        ax = plt.gca()
+
+    if isinstance(shp, list):
+        n_shapes = len(shp)
+        if color is None or len(color) != n_shapes:
+            color = [f"C{k}" for k in range(n_shapes)]
+        for k,s in enumerate(shp):
+            gs = gpd.GeoSeries(s)
+            gs.plot(ax=ax, color=color[k])
+    else:
+        gs = gpd.GeoSeries(shp, color=color)
+        gs.plot(ax=ax)
+
+    if invert_y:
+        ax.invert_yaxis()
+
+    if show:
         plt.grid()
         plt.axis('equal')
         plt.show(block=True)
-    else:
-        return p
+
 
