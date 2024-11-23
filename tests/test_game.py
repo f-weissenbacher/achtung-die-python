740894b18bba9248d321bb3ba17f536c25e0dd34
Fabian Weissenbacher <40057494+f-weissenbacher@users.noreply.github.com>
2024-11-24 00:10:33 +0100

    repeatability test

--- tests/test_game.py
+++ tests/test_game.py
@@ -1,18 +1,40 @@
 from unittest import TestCase
 
+from develop.predict_trails import dist_per_tick
 from game import AchtungDieKurveGame
 from players import Player
 
 from numpy import allclose
 
+from players.aiplayers import RandomSteeringAIPlayer
+
+
 class TestAchtungDieKurveGame(TestCase):
-    def test_random_seed(self):
-        rng_seed = 1234
+    def test_repeatability(self):
+        rng_seed = 123456
 
-        game = AchtungDieKurveGame(mode="headless", rng_seed=rng_seed)
-        game.initialize_players([1,2])
+        def setup_and_run_game(seed):
+            game = AchtungDieKurveGame(mode="headless", rng_seed=seed, target_fps=10)
+            player_kwargs = {"turn_angles_deg": (10., 50.), "straight_lengths": (0, 30.)}
+            game.spawn_player(1, player_type=RandomSteeringAIPlayer, **player_kwargs)
+            game.spawn_player(2, player_type=RandomSteeringAIPlayer, **player_kwargs)
+            for k in range(50):
+                game.tick_forward()
+            return game.get_game_state()
 
-        p1_reference = Player(1, init_pos=, init_angle=)
+        game_a_state = setup_and_run_game(rng_seed)
+        game_b_state = setup_and_run_game(rng_seed)
+
+        # Compare trails and angles for player 1
+        self.assertTrue(allclose(game_a_state[1]['trail'], game_b_state[1]['trail']))
+        self.assertTrue(allclose(game_a_state[1]['angles'], game_b_state[1]['angles']))
+
+        # Compare trails and angles for player 2
+        self.assertTrue(allclose(game_a_state[2]['trail'], game_b_state[2]['trail']))
+        self.assertTrue(allclose(game_a_state[2]['angles'], game_b_state[2]['angles']))
+
+
+
 
 
     def test_detect_wall_collision(self):
